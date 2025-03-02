6cd6e194006f46c6c10320e31a1ab18a3b1957ed
Andrea Vasquez <ajvasquezb@unah.hn>
2025-03-02 17:23:26 -0600

    Add JSON middleware and sample route to backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,3 +1,14 @@
-import express from "express";
+import express, {json} from "express";
+import {Keys} from "./keys";
 
 const app = express();
+
+app.use(json());
+
+app.use("/", (req, res) => {
+    res.json({message: "Probando una ruta random en el backend"})
+})
+
+app.listen(Keys.Port, ()=> {
+    console.log(`Server running on port ${Keys.Port}`);
+})
