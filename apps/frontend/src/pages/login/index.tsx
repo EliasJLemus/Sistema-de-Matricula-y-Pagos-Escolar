1a41878ddc6d5855662e0584ac53ab326ecab5a2
Andrea Vasquez <ajvasquezb@unah.hn>
2025-03-02 21:53:22 -0600

    Refactor CSS styles for layout and update login page structure

--- apps/frontend/src/pages/login/index.tsx
+++ apps/frontend/src/pages/login/index.tsx
@@ -3,30 +3,36 @@ import { Box, Button, Container, Grid, Paper, TextField, Typography } from "@mui
 
 export const LoginPage: React.FC = () => {
     return (
-        <Container maxWidth="xl">
-            <Grid container 
-            direction="row" 
-            alignItems={"center"} 
-            justifyContent={"center"} 
-            style={{ height: "100vh", minHeight: "100vh" }}>
+       <Container
+        maxWidth={false}
+        sx={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            backgroundColor: "#f7f7f7b8",
+            padding: "0px",
+            margin: "0px"
+        }}
+        >
+            
+            <Box
+            boxShadow={3} p={3} 
+            bgcolor="#1B1263" 
+            sx={{
+                width: "35%", 
+                maxWidth: "100%",
+                "borderTopRightRadius": "50px",
+                "borderBottomRightRadius": "50px",
+            }}
+            height="100vh"
+            margin="0px"
+            position={"absolute"}
+            left={"0"}
+            >
 
-                <Grid item>
-                    <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
-                        <Typography variant='h4'>
-                            Iniciar Sesion
-                        </Typography>
-                        <Box component="form">
-                            <TextField />
-                            <TextField />
-                            <Button type='submit'>
-                                Iniciar Sesion
-                            </Button>
-                        </Box>
+            </Box>
 
-                    </Paper>
-                </Grid>
-
-            </Grid>
-        </Container>
+       </Container>
     )
 }
