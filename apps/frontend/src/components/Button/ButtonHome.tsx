9df157efb4a3f8cb3bbf22b17ea5c2cd1eb65077
Andrea Vasquez <judithvasquezdev@gmail.com>
2025-03-18 13:17:19 -0600

    Style ButtonHome component with custom styles for improved appearance and hover effect

--- apps/frontend/src/components/Button/ButtonHome.tsx
+++ apps/frontend/src/components/Button/ButtonHome.tsx
@@ -14,6 +14,21 @@ const ButtonHome: React.FC<ButtonHomeProps> = ({
 }) => {
     return (
         <Button 
+         sx={
+            {
+                backgroundColor: "#F5F1E3",
+                width: '100%',
+                height: '40px',
+                borderRadius: '0px',
+                textTransform: 'none',
+                justifyContent: 'flex-start',
+                paddingLeft: '20px',
+                color: 'white',
+                '&:hover': {
+                    backgroundColor: 'rgba(255, 255, 255, 0.2)'
+                }
+            }
+         }
         {...props}
         startIcon = {
             <img src={img} alt={imgAlt} />
