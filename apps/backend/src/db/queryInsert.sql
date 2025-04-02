a86ef18e8c7d83471a00ad832db5bdce97fa8a04
Andrea Vasquez <judith.vasquez@graphxsource.hn>
2025-04-02 10:57:45 -0600

    Add initial data inserts for InformacionAcademica, Encargados, and Grados tables

---
+++ apps/backend/src/db/queryInsert.sql
@@ -0,0 +1,110 @@
+-- Insertar datos en la tabla "InformacionAcademica"
+INSERT INTO "Estudiantes"."InformacionGeneral" (
+    uuid, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido,
+    nacionalidad, identidad, genero, fecha_nacimiento, edad, direccion, tipo_persona
+) VALUES
+('8f853b6a-8f63-4a91-86a1-94e6ecc3e7e0', 'Carlos', 'Enrique', 'Ramírez', 'González',
+ 'Hondureña', '0801-1990-1000', 'Masculino', '2015-10-10', 10, 'Aldea El Ocotal, Danlí', 'Estudiante'),
+
+('79f65551-6e1e-44c0-8cde-1fc656b29ed2', 'Carlos', 'Enrique', 'Ramírez', 'González',
+ 'Hondureña', '0801-1991-1001', 'Masculino', '1983-07-05', 42, 'Colonia Las Palmas, Juticalpa', 'Encargado'),
+
+('ac5b5357-638a-4801-9886-1d25a991a1e2', 'Ana', 'Lucía', 'Pérez', 'Martínez',
+ 'Hondureña', '0801-1992-1002', 'Femenino', '2012-04-18', 13, 'Barrio El Calvario, Santa Rosa de Copán', 'Estudiante'),
+
+('b422d8c6-8d3a-4775-9c85-b44d8bd27f18', 'Ana', 'Lucía', 'Pérez', 'Martínez',
+ 'Hondureña', '0801-1993-1003', 'Femenino', '1994-11-01', 31, 'Colonia El Pedregal, San Pedro Sula', 'Encargado'),
+
+('247dd6ba-4731-44d0-8621-4db519fd26e4', 'José', 'Luis', 'Hernández', 'Lopez',
+ 'Hondureña', '0801-1994-1004', 'Masculino', '2016-10-23', 9, 'Colonia El Carmen, Gracias', 'Estudiante'),
+
+('ea99cb84-f610-49d3-8ad3-0f3a466bcb87', 'José', 'Luis', 'Hernández', 'Lopez',
+ 'Hondureña', '0801-1995-1005', 'Masculino', '1984-06-22', 41, 'Colonia Satélite, Choloma', 'Encargado'),
+
+('a4dc7a92-7265-4e76-a2e1-3c7c0d86f63f', 'María', 'Fernanda', 'Cruz', 'Zelaya',
+ 'Hondureña', '0801-1996-1006', 'Femenino', '2013-08-01', 12, 'Barrio Abajo, La Ceiba', 'Estudiante'),
+
+('90f97f56-2173-4d3b-a8d5-bcb8cc6750fd', 'María', 'Fernanda', 'Cruz', 'Zelaya',
+ 'Hondureña', '0801-1997-1007', 'Femenino', '1992-10-26', 33, 'Barrio El Benque, Comayagua', 'Encargado'),
+
+('9eeb8b5b-f45e-4b29-9444-e7ee46ab7564', 'Daniel', 'Esteban', 'Castro', 'Flores',
+ 'Hondureña', '0801-1998-1008', 'Masculino', '2011-07-25', 14, 'Colonia Kennedy, Tegucigalpa', 'Estudiante'),
+
+('f84e6808-bf00-4c67-a403-f7f7d3563697', 'Daniel', 'Esteban', 'Castro', 'Flores',
+ 'Hondureña', '0801-1999-1009', 'Masculino', '1990-08-11', 35, 'Barrio El Centro, Tegucigalpa', 'Encargado'),
+
+('6c1478b8-b029-4424-a12f-2ac80be21d12', 'Karla', 'Patricia', 'Reyes', 'Mejía',
+ 'Hondureña', '0801-2000-1010', 'Femenino', '2012-01-06', 13, 'Colonia El Pedregal, San Pedro Sula', 'Estudiante'),
+
+('27d93c1e-3e31-4725-aaa2-fffe31e770e0', 'Karla', 'Patricia', 'Reyes', 'Mejía',
+ 'Hondureña', '0801-2001-1011', 'Femenino', '1991-05-08', 34, 'Barrio El Calvario, Santa Rosa de Copán', 'Encargado'),
+
+('a8d4df19-3d8b-40e5-bfab-40c1de3b008c', 'Luis', 'Alonso', 'Suazo', 'García',
+ 'Hondureña', '0801-2002-1012', 'Masculino', '2010-04-04', 15, 'Colonia Las Palmas, Juticalpa', 'Estudiante'),
+
+('30893d7f-3da0-4c24-985c-f99e85c43866', 'Luis', 'Alonso', 'Suazo', 'García',
+ 'Hondureña', '0801-2003-1013', 'Masculino', '1985-03-30', 40, 'Aldea El Ocotal, Danlí', 'Encargado'),
+
+('2514f9e2-ff9b-47fc-bd5e-e4fa90a89c69', 'Fernanda', 'Isabel', 'Ortiz', 'Cálix',
+ 'Hondureña', '0801-2004-1014', 'Femenino', '2016-09-01', 9, 'Colonia Satélite, Choloma', 'Estudiante'),
+
+('32c5dc6a-7128-4a1b-9634-0b0848e33286', 'Fernanda', 'Isabel', 'Ortiz', 'Cálix',
+ 'Hondureña', '0801-2005-1015', 'Femenino', '1982-11-19', 43, 'Barrio Abajo, La Ceiba', 'Encargado'),
+
+('bf3f2a7b-c1f2-44b2-802e-cc67d75d3814', 'Jorge', 'Emilio', 'Barahona', 'Ramos',
+ 'Hondureña', '0801-2006-1016', 'Masculino', '2014-02-11', 11, 'Colonia El Carmen, Gracias', 'Estudiante'),
+
+('36d3152c-3f84-46f2-a53b-41b95612a9a3', 'Jorge', 'Emilio', 'Barahona', 'Ramos',
+ 'Hondureña', '0801-2007-1017', 'Masculino', '1981-09-14', 44, 'Barrio El Benque, Comayagua', 'Encargado'),
+
+('001f9a4a-0618-471e-a011-0c4f6db21e32', 'Gabriela', 'Sofía', 'Figueroa', 'Mendoza',
+ 'Hondureña', '0801-2008-1018', 'Femenino', '2011-03-14', 14, 'Colonia Kennedy, Tegucigalpa', 'Estudiante'),
+
+('c23e7923-dc36-4d7a-8055-8a3035cf06f3', 'Gabriela', 'Sofía', 'Figueroa', 'Mendoza',
+ 'Hondureña', '0801-2009-1019', 'Femenino', '1980-01-26', 45, 'Barrio El Centro, Tegucigalpa', 'Encargado');
+
+--tabla encargado
+INSERT INTO "Estudiantes"."Encargados" (
+    uuid, numero_encargado, uuid_info_general, correo_electronico, telefono
+) VALUES
+('e85cc879-bf01-41d7-9a28-109028a57779', 1, '79f65551-6e1e-44c0-8cde-1fc656b29ed2', 'carlos.ramírez1@correo.hn', '99915646'),
+('5e489d3e-2a15-442a-983b-11e7fa11685f', 2, 'b422d8c6-8d3a-4775-9c85-b44d8bd27f18', 'ana.pérez2@correo.hn', '99542785'),
+('61e8dd38-348d-4eb5-a7b2-3e8b3c9270ac', 3, 'ea99cb84-f610-49d3-8ad3-0f3a466bcb87', 'josé.hernández3@correo.hn', '98293164'),
+('9e41aa1b-4f65-4391-9aed-b3f5a2e3890d', 4, '90f97f56-2173-4d3b-a8d5-bcb8cc6750fd', 'maría.cruz4@correo.hn', '93194133'),
+('1a9bf71e-27ae-45f0-9efb-add8d1d21758', 5, 'f84e6808-bf00-4c67-a403-f7f7d3563697', 'daniel.castro5@correo.hn', '98065496'),
+('c6b2a948-dcf1-4200-b7b5-5026a1bcbe16', 6, '27d93c1e-3e31-4725-aaa2-fffe31e770e0', 'karla.reyes6@correo.hn', '98910279'),
+('20551e69-9f1c-4426-b259-991d8cfebbe0', 7, '30893d7f-3da0-4c24-985c-f99e85c43866', 'luis.suazo7@correo.hn', '96839833'),
+('fc8d175f-27c0-4a3c-9e18-13539e7599c4', 8, '32c5dc6a-7128-4a1b-9634-0b0848e33286', 'fernanda.ortiz8@correo.hn', '98382861'),
+('a3ed1b6c-8261-4a34-900b-d9a8deaf14d0', 9, '36d3152c-3f84-46f2-a53b-41b95612a9a3', 'jorge.barahona9@correo.hn', '93303539'),
+('b66b3434-4162-419e-9fe6-0db9d4341c6d', 10, 'c23e7923-dc36-4d7a-8055-8a3035cf06f3', 'gabriela.figueroa10@correo.hn', '95199126');
+
+--Tabla grado
+INSERT INTO "Administracion"."Grados" (
+    uuid, nombre_grado, seccion, capacidad_maxima, year_electivo, nivel
+) VALUES
+('f9bdd66f-c33d-44c2-b977-652fa52e93ee', 'Jardín', 'A', 30, 2025, 'Pre-Basica'),
+('08c58d12-5384-4554-b9f2-7bd003d6d9f3', 'Jardín', 'B', 30, 2025, 'Pre-Basica'),
+('4049270c-bb7e-4de9-ac7f-e6b2cf6d3c5c', 'Preparatoria', 'A', 30, 2025, 'Pre-Basica'),
+('f8f98fbb-ed7e-45bb-927f-ef7cb43b6bd7', 'Preparatoria', 'B', 30, 2025, 'Pre-Basica'),
+('c97528f6-6102-4f11-aaea-4da30386b5fa', 'Primero', 'A', 35, 2025, 'Basica'),
+('cb4c396b-9e34-45a5-83a7-2d5184f36c16', 'Primero', 'B', 35, 2025, 'Basica'),
+('30d8cb2d-68e0-4a48-a71b-86950d23b8ce', 'Segundo', 'A', 35, 2025, 'Basica'),
+('208fdf3e-b9b7-4872-bdf8-4a63ddaa3ddf', 'Segundo', 'B', 35, 2025, 'Basica'),
+('a4b1cc2d-c92a-4e9e-a01a-c3ed800ec62f', 'Tercero', 'A', 35, 2025, 'Basica'),
+('a27a58e1-0f33-4ea3-8667-c95d29fcf4cc', 'Tercero', 'B', 35, 2025, 'Basica'),
+('6126c7a9-c004-403e-a395-d25b0fcf2e77', 'Cuarto', 'A', 30, 2025, 'Basica'),
+('b215ed50-c861-4bc2-99c6-2b1dcb3905c6', 'Cuarto', 'B', 30, 2025, 'Basica'),
+('d7f35cc2-e48c-4a06-8a49-9a5f93892b9e', 'Quinto', 'A', 30, 2025, 'Basica'),
+('fa8e34b0-23bc-4b42-a240-f6aa3d8a6e8f', 'Quinto', 'B', 30, 2025, 'Basica'),
+('f0243c85-14f5-4abf-8f7d-e3bcff2923e2', 'Sexto', 'A', 30, 2025, 'Basica'),
+('8fd6348c-74c4-48ff-a3d1-68e13f56cf5e', 'Sexto', 'B', 30, 2025, 'Basica'),
+('63a2958d-65b6-4d21-85fc-2880d6206b6c', 'Séptimo', 'A', 25, 2025, 'Secundaria'),
+('777c2ae2-e15b-49f6-929c-d4c6b961ed2e', 'Séptimo', 'B', 25, 2025, 'Secundaria'),
+('b5b24e51-7ac7-47fc-a0ee-78f37894adf1', 'Octavo', 'A', 25, 2025, 'Secundaria'),
+('54a03725-c3e5-41a4-bbc6-ccbaad56f96c', 'Octavo', 'B', 25, 2025, 'Secundaria'),
+('e395b9e1-347b-40f9-8ac0-f1decd1c5573', 'Noveno', 'A', 25, 2025, 'Secundaria'),
+('f290a224-2d66-4321-980e-826c184eb7ce', 'Noveno', 'B', 25, 2025, 'Secundaria'),
+('3f73fc35-3df5-463f-a8d6-0161ec745be1', 'Décimo', 'A', 20, 2025, 'Secundaria'),
+('85833408-7184-4075-a5b5-b03052e87c89', 'Décimo', 'B', 20, 2025, 'Secundaria'),
+('43e00f3f-9735-4096-9f1a-2f68a77513ea', 'Undécimo', 'A', 20, 2025, 'Secundaria'),
+('2584c407-59fd-45fd-9a34-167197b2db11', 'Undécimo', 'B', 20, 2025, 'Secundaria');
