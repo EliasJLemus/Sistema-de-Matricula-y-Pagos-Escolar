4dfe380c0e7800aecff4918f099d93ddfa70bf9f
Andrea Vasquez <judith.vasquez@graphxsource.hn>
2025-04-02 11:06:03 -0600

    Add initial data inserts for Estudiantes and EstudianteEncargado tables

--- apps/backend/src/db/queryInsert.sql
+++ apps/backend/src/db/queryInsert.sql
@@ -108,3 +108,55 @@ INSERT INTO "Administracion"."Grados" (
 ('85833408-7184-4075-a5b5-b03052e87c89', 'Décimo', 'B', 20, 2025, 'Secundaria'),
 ('43e00f3f-9735-4096-9f1a-2f68a77513ea', 'Undécimo', 'A', 20, 2025, 'Secundaria'),
 ('2584c407-59fd-45fd-9a34-167197b2db11', 'Undécimo', 'B', 20, 2025, 'Secundaria');
+
+--Tabla: estudiante
+INSERT INTO "Estudiantes"."Estudiantes" (
+    uuid, numero_estudiante, uuid_info_general, uuid_grado,
+    es_zurdo, dif_educacion_fisica, reaccion_alergica,
+    descripcion_alergica, fecha_admision, estado
+) VALUES
+('13af34a6-2d0a-46ff-aa15-ea79ed23b669', 1001, '8f853b6a-8f63-4a91-86a1-94e6ecc3e7e0',
+ 'a4b1cc2d-c92a-4e9e-a01a-c3ed800ec62f', true, false, false, 'Alergia a polvo', '2025-02-14', 'Activo'),
+
+('f6f70129-9c17-4fef-8563-e48d2193239c', 1002, 'ac5b5357-638a-4801-9886-1d25a991a1e2',
+ '208fdf3e-b9b7-4872-bdf8-4a63ddaa3ddf', true, true, false, NULL, '2025-02-21', 'Activo'),
+
+('c9312b0e-de1b-49be-b92f-30b08fa99ef1', 1003, '247dd6ba-4731-44d0-8621-4db519fd26e4',
+ 'a27a58e1-0f33-4ea3-8667-c95d29fcf4cc', true, false, false, NULL, '2025-01-22', 'Activo'),
+
+('32b6da38-77df-4247-90cf-92feea526ddc', 1004, 'a4dc7a92-7265-4e76-a2e1-3c7c0d86f63f',
+ 'a27a58e1-0f33-4ea3-8667-c95d29fcf4cc', true, false, true, 'Alergia a polvo', '2025-02-19', 'Activo'),
+
+('b2cabf90-28e4-454f-96d6-ee3b10a27a2f', 1005, '9eeb8b5b-f45e-4b29-9444-e7ee46ab7564',
+ '208fdf3e-b9b7-4872-bdf8-4a63ddaa3ddf', false, false, false, NULL, '2025-03-26', 'Activo'),
+
+('4e49337d-2072-4b15-81b1-2c8c38c3e6f1', 1006, '6c1478b8-b029-4424-a12f-2ac80be21d12',
+ 'cb4c396b-9e34-45a5-83a7-2d5184f36c16', false, false, false, NULL, '2025-02-02', 'Activo'),
+
+('13d7b313-3650-4b3f-9c66-eebff45d2bc1', 1007, 'a8d4df19-3d8b-40e5-bfab-40c1de3b008c',
+ 'c97528f6-6102-4f11-aaea-4da30386b5fa', true, false, false, NULL, '2025-02-17', 'Activo'),
+
+('861c96c3-c29b-4a30-9477-54a401ba58a7', 1008, '2514f9e2-ff9b-47fc-bd5e-e4fa90a89c69',
+ 'cb4c396b-9e34-45a5-83a7-2d5184f36c16', false, true, true, 'Alergia a polvo', '2025-01-25', 'Activo'),
+
+('0f132f38-e097-4b7e-b002-bd03e4ce45ff', 1009, 'bf3f2a7b-c1f2-44b2-802e-cc67d75d3814',
+ 'a4b1cc2d-c92a-4e9e-a01a-c3ed800ec62f', false, false, false, NULL, '2025-01-14', 'Activo'),
+
+('3cf7990d-64b6-4594-bb6c-4fdd7f8c826d', 1010, '001f9a4a-0618-471e-a011-0c4f6db21e32',
+ '30d8cb2d-68e0-4a48-a71b-86950d23b8ce', false, false, false, NULL, '2025-02-23', 'Activo');
+
+
+--Tabla EstudianteEncargado
+INSERT INTO "Estudiantes"."EstudianteEncargado" (
+    uuid_estudiante, uuid_encargado, parentesco, es_principal
+) VALUES
+('13af34a6-2d0a-46ff-aa15-ea79ed23b669', 'e85cc879-bf01-41d7-9a28-109028a57779', 'Padre', TRUE),
+('f6f70129-9c17-4fef-8563-e48d2193239c', '5e489d3e-2a15-442a-983b-11e7fa11685f', 'Madre', TRUE),
+('c9312b0e-de1b-49be-b92f-30b08fa99ef1', '61e8dd38-348d-4eb5-a7b2-3e8b3c9270ac', 'Padre', TRUE),
+('32b6da38-77df-4247-90cf-92feea526ddc', '9e41aa1b-4f65-4391-9aed-b3f5a2e3890d', 'Madre', TRUE),
+('b2cabf90-28e4-454f-96d6-ee3b10a27a2f', '1a9bf71e-27ae-45f0-9efb-add8d1d21758', 'Padre', TRUE),
+('4e49337d-2072-4b15-81b1-2c8c38c3e6f1', 'c6b2a948-dcf1-4200-b7b5-5026a1bcbe16', 'Madre', TRUE),
+('13d7b313-3650-4b3f-9c66-eebff45d2bc1', '20551e69-9f1c-4426-b259-991d8cfebbe0', 'Padre', TRUE),
+('861c96c3-c29b-4a30-9477-54a401ba58a7', 'fc8d175f-27c0-4a3c-9e18-13539e7599c4', 'Tía', TRUE),
+('0f132f38-e097-4b7e-b002-bd03e4ce45ff', 'a3ed1b6c-8261-4a34-900b-d9a8deaf14d0', 'Abuelo', TRUE),
+('3cf7990d-64b6-4594-bb6c-4fdd7f8c826d', 'b66b3434-4162-419e-9fe6-0db9d4341c6d', 'Madre', TRUE);
